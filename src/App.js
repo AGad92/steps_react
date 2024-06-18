543aafe0be1b6864a1babb2f6676a163e7faab43
Ahmed Gad <ahmed.gad19@hotmail.com>
2024-06-18 20:03:08 +0300

    completed app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
-function App() {
+export default function App() {
+  // component state
+  const [step, setStep] = useState(1);
+  const [close, setClose] = useState(false);
+
+  // Step handlers
+  function handlePrevious() {
+    if (step === 1) return;
+    setStep(step - 1);
+  }
+
+  function handleNext() {
+    if (step === 3) return;
+    setStep(step + 1);
+  }
+
+  function handleClose() {
+    setClose(!close);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <span className="close" onClick={handleClose}>
+        &times;
+      </span>
+      <div className="steps" style={{ display: close ? "none" : "block" }}>
+        <div className="numbers">
+          <div className={step >= 1 ? "active" : undefined}>1</div>
+          <div className={step >= 2 ? "active" : undefined}>2</div>
+          <div className={step >= 3 ? "active" : undefined}>3</div>
+        </div>
+        <p className="message">
+          Step {step}: {messages[step - 1]}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="buttons">
+          <button
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            onClick={handlePrevious}
+          >
+            Previous
+          </button>
+          <button
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            onClick={handleNext}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
